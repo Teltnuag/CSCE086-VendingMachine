15d718cfb8004b17f29eea509bb5f989adf8df3a
Mason Wright <mason.wright@afit.edu>
2022-09-15 14:48:08 -0400

    Basic setup

---
+++ VendingMachine.py
@@ -0,0 +1,45 @@
+# Show the balance
+def balance():
+
+    return
+
+# Prints list of transcations
+def history():
+
+    return
+
+# Prints available items with name and ID
+def inventory():
+
+    return
+
+# Add an item: name, quantity, price
+def add_item(name, quantity, price):
+
+    return
+
+# Buys an item with # dollars, quarters, dimes, nickles,
+# pennies. It also shows change given and the remaining
+# balance with currency distribution. For change, the machine
+# uses the largest denominator of curenncy that is available.
+def buy_item(name, dollars, quarters, dimes, nickles, pennies):
+
+    return
+
+# Displays help menu with these commands.
+def help():
+
+    return
+
+# Exit the vending machine.
+def exit():
+
+    return
+
+# Main command function
+def main():
+    print("Super Vending Machine Activated")
+    return
+
+if __name__ == "__main__":
+    main()
